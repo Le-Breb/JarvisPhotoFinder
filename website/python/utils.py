7646bd65b01ed9b6c5e32c8b705ade7c19d45047
Mathieu Brebion <mathieu.brebion@epita.fr>
2025-11-03 22:08:46 +0100

    added bmp to list of image extensions

--- website/python/utils.py
+++ website/python/utils.py
@@ -3,7 +3,7 @@ import glob
 import os
 
 def get_images(relative: bool):
-    images = [element for element in glob.iglob(os.path.join(config.IMAGES_FOLDER, '**', '**'), recursive=True) if element.lower().endswith((".jpg", ".jpeg", ".png", ".webp"))]
+    images = [element for element in glob.iglob(os.path.join(config.IMAGES_FOLDER, '**', '**'), recursive=True) if element.lower().endswith((".jpg", ".jpeg", ".png", ".webp", ".bmp"))]
     if relative:
         images = [os.path.relpath(path, config.IMAGES_FOLDER) for path in images]
 
