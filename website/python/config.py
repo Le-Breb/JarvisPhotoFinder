69abebc73a12b5ba9af8648150f50d29fcf9cb3a
Mathieu Brebion <mathieu.brebion@epita.fr>
2025-11-03 22:45:54 +0100

    added image folder blacklist support + fix images glob duplicates

--- website/python/config.py
+++ website/python/config.py
@@ -1,3 +1,5 @@
 import os
 
 IMAGES_FOLDER=os.path.join(os.path.abspath(os.path.curdir), "images")
+IMAGES_BLACKLIST_FILE='images_blacklist.txt'
+HANDLED_IMG_EXTENSIONS=(".jpg", ".jpeg", ".png", ".webp", ".bmp")
--- website/python/utils.py
+++ website/python/utils.py
@@ -3,8 +3,17 @@ import glob
 import os
 
 def get_images(relative: bool):
-    images = [element for element in glob.iglob(os.path.join(config.IMAGES_FOLDER, '**', '**'), recursive=True) if element.lower().endswith((".jpg", ".jpeg", ".png", ".webp", ".bmp"))]
+    # Get images
+    images = [element for element in glob.iglob(os.path.join(config.IMAGES_FOLDER, '**'), recursive=True) if element.lower().endswith(config.HANDLED_IMG_EXTENSIONS)]
+
+    # Convert path to relative path (relative to IMAGE_FOLDER)
     if relative:
         images = [os.path.relpath(path, config.IMAGES_FOLDER) for path in images]
+
+    # Filter undesired images according to the ones listed in blacklist file
+    if os.path.exists(config.IMAGES_BLACKLIST_FILE):
+        with open(config.IMAGES_BLACKLIST_FILE, 'r') as f:
+            for file in f.readlines():
+                images = [img for img in images if not file.strip() in img]
 
     return images
